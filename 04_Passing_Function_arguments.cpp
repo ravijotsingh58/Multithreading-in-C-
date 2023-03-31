a3f69d219503aca1fd8bf0a9b6f668dcbb47aaf6
Polynomial <Ravijotsingh427@gmail.com>
2023-03-31 12:42:10 +0530

    Passing arguments to function as reference

---
+++ 04_Passing_Function_arguments.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<thread>
+#include<mutex>
+using namespace std;
+
+/*
+in C++, if we pass arguments directly without std::ref or std::reference_wrapper, the arguments will be copied 
+into new thread rather than being passed by reference. when variable is copied into new thread, it creates a new
+instance of the variable in the new thread's memory space. This means that any modifications made to the variable
+in the new thread will not be reflected in the original variable in the main thread.
+*/
+
+void performIncr(int& count, mutex& mtx){
+    
+    for(int i = 1;i <= 1E6; i++){
+        mtx.lock();
+        ++count;
+        mtx.unlock();
+    }
+}
+
+int main(int argc, char** argv){
+
+    int count = 0;
+    mutex mtx;
+    
+    thread th1{performIncr, ref(count), ref(mtx)};
+    thread th2{performIncr, ref(count), ref(mtx)};
+
+    th1.join(); th2.join();
+
+    cout<<"Final count: "<<count;
+    return 0;
+}
